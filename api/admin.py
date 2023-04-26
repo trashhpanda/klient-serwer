3978ed0fb34585aa614bab65ccad3d2c28354b79
Sam <249195@student.pwr.edu.pl>
2023-04-26 23:00:55 +0200

    dostęp do modeli w panelu admina Django + dodanie superuserów do bazy

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+# modele z aplikacji api
+from .models import Student, Language, Qualification, Instructor, Address, School, ClassType, Calendar, Booking
 
-# Register your models here.
+# dostęp do modeli z panelu admina
+admin.site.register(Student)
+admin.site.register(Language)
+admin.site.register(Qualification)
+admin.site.register(Instructor)
+admin.site.register(Address)
+admin.site.register(School)
+admin.site.register(ClassType)
+admin.site.register(Calendar)
+admin.site.register(Booking)
